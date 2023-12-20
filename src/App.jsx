d0353569236c226e2526a5b40213ec0570f8542d
phumkrit supkavnich <phumkrit.s@cargolink.co.th>
2023-12-20 15:00:24 +0700

    add component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookies from "universal-cookie";
+import "./App.css";
+
+import { ChannelContainer, ChannelListContainer } from "./components";
+
+const apiKey = "j48bsujr7w7f";
+
+const client = StreamChat.getInstance(apiKey);
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div className="app__wrapper">
+      <Chat client={client} theme="team light">
+        <ChannelListContainer />
+        <ChannelContainer />
+      </Chat>
     </div>
   );
 };
